6717e8071594018dc7aaed8b29fa6fe204464c6f
FerrariRoma <win9592@gmail.com>
2022-07-17 18:07:37 +0900

    Study : fetch

--- network_ex/src/App.js
+++ network_ex/src/App.js
@@ -3,11 +3,10 @@ import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
-    let xhr = new XMLHttpRequest();
+    /* let xhr = new XMLHttpRequest();
     console.log(xhr);
     xhr.open("GET", "http://localhost:5001/sleep_times");
-    xhr.send();
-
+    xhr.send(); */
     // XMLHttpRequest.readyState는 XMLHttpRequest의 상태를 반환해준다. 완벽한 상태가 아니라도 계속 호출을 한다.
     /* 
     0~4번까지
@@ -17,19 +16,36 @@ function App() {
     3 : 응답을 기다리는 상태
     4 : 응답을 받은 상태 => 데이터를 가지고 올 수 있다.
     */
-
     // 바뀔 때마다 콜백함수를 호출해준다.
     /* xhr.onreadystatechange = () => {
       if (xhr.readyState === 4) {
         console.log(xhr.responseText);
       }
     }; */
-
     // onload는 완벽히 서버의 응답이 끝났을 때 실행이 된다.
-    xhr.onload = () => {
+    /*     xhr.onload = () => {
       console.log(xhr.responseText);
-    };
+    }; */
+
+    callback();
   });
+
+  // fetch는 promise를 반환해준다.
+  const callback = async () => {
+    const data = {
+      day: "일",
+      sleep_time: "9:00",
+    };
+    const response = await fetch("http://localhost:5001/sleep_times", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(data),
+    });
+    console.log(response);
+  };
+
   return (
     <div className="App">
       <p>App.js</p>
