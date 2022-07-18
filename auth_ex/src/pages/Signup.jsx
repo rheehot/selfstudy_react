0de5835117bec3b85c70144a724840bca269417e
FerrariRoma <win9592@gmail.com>
2022-07-18 16:53:03 +0900

    Study: firebase's Authentication

---
+++ auth_ex/src/pages/Signup.jsx
@@ -0,0 +1,41 @@
+import { useRef } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../shared/firebase";
+
+const Signup = () => {
+  const refId = useRef(null);
+  const refPw = useRef(null);
+  const refName = useRef(null);
+
+  const signupBtn = async () => {
+    console.log(refId.current.value);
+    console.log(refPw.current.value);
+    console.log(refId.current.value);
+    const new_user = await createUserWithEmailAndPassword(
+      getAuth(),
+      refId.current.value,
+      refId.current.value
+    );
+    await addDoc(collection(db, "authex"), {
+      user_id: new_user.user.email,
+      name: refId.current.value,
+    });
+  };
+  return (
+    <>
+      <div>
+        아이디: <input ref={refId} type="email" placeholder="e-mail"></input>{" "}
+        <br />
+        비밀번호:{" "}
+        <input ref={refPw} type="password" placeholder="password"></input>{" "}
+        <br />
+        이름: <input ref={refName} type="text" placeholder="name"></input>{" "}
+        <br />
+        <button onClick={signupBtn}>회원가입</button>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
---
+++ auth_ex/src/shared/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: "authex-c2dd6",
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// init firestore db
+export const db = getFirestore(app);
+
+export default app;
