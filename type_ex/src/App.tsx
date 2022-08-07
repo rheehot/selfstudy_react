9cc080c0e8809b1a86ce796b987f35907b24b885
FerrariRoma <win9592@gmail.com>
2022-08-07 18:48:27 +0900

    Event에 대한 type지정

--- type_ex/src/App.tsx
+++ type_ex/src/App.tsx
@@ -1,11 +1,32 @@
-import React from "react";
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  // 원래 event는 any타입이 설정 되어 있는데, 항상 ts를 사용할 땐 any타입을 사용하지 않으려고 노력해야 한다.
+  // 그래서 우린 ReactJS 내에 있는 type을 지정해주는데, 바로 React.FormEvent이다.
+  // what kind of element can take onChange event
+  const handleInput = (e: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = e;
+    return setUsername(value);
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("hello", username);
+    return setUsername("");
+  };
   return (
     <div>
-      <Circle bgColor="teal" borderColor="blue" />
-      <Circle bgColor="tomato" text="Test Text" />
+      <form onSubmit={handleSubmit}>
+        <input
+          value={username}
+          onChange={handleInput}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
