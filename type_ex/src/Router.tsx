71b1fb983d9b51b379869941afbbc7bd2841cb1c
FerrariRoma <win9592@gmail.com>
2022-08-08 11:02:12 +0900

    Coin page에서 Router추가

--- type_ex/src/Router.tsx
+++ type_ex/src/Router.tsx
@@ -2,13 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Coin from "./routes/Coin";
 import Home from "./routes/Home";
 import Header from "./routes/Header";
+import Chart from "./routes/Chart";
+import Price from "./routes/Price";
+
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Header />}>
           <Route index element={<Home />} />
-          <Route path="/:coinId" element={<Coin />} />
+          <Route path="/:coinId" element={<Coin />}>
+            <Route index element={<Price />} />
+            <Route path="chart" element={<Chart />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
--- type_ex/src/routes/Coin.tsx
+++ type_ex/src/routes/Coin.tsx
@@ -1,5 +1,29 @@
-import { useParams } from "react-router";
+import { useEffect, useState } from "react";
+import { useParams, Outlet } from "react-router";
 import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
 
 interface RouteState {
   state: {
@@ -7,15 +31,123 @@ interface RouteState {
   };
 }
 
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
 function Coin() {
   // useParams에 default타입이 string|undefined라서 안해줘 됨, 하지만 해줄 거면 useState에 하는 것을 참고해서 해보자.
   // const [ex, setEx] = useState<interfaceObject[]>([])
   // const [ex, setEx] = useParams<RouteParams>([])
+  const { coinId } = useParams();
   const { state } = useLocation() as RouteState;
-  console.log(state);
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      console.log(infoData);
+      console.log(priceData);
+      setInfo(infoData);
+      setPriceInfo(priceData);
+      setLoading(false);
+    })();
+  }, []);
   return (
     <>
-      <h1>coin {state.name || "...Loading"}</h1>
+      <h1>
+        coin {state?.name ? state.name : loading ? "...Loading" : info?.name}
+      </h1>
+      {loading ? (
+        "Loading..."
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          <Outlet />
+        </>
+      )}
     </>
   );
 }
