1789ad963309c8c78874f6c489bcef12f946815f
FerrariRoma <win9592@gmail.com>
2022-08-23 21:43:57 +0900

    js doc 공부

--- typescript/typechain/src/index.ts
+++ typescript/typechain/src/index.ts
@@ -1,5 +1,5 @@
-import { init, exit } from "myPackage";
-// import { init, exit } from "./myPackage"; // 타입스크립트 파일에 "./myPackage"파일을 불러온다는 뜻이다.
+// import { init, exit } from "myPackage";
+import { init, exit } from "./myPackage"; // 타입스크립트 파일에 "./myPackage"파일을 불러온다는 뜻이다.
 
 init({ url: "true" });
 exit(1);
