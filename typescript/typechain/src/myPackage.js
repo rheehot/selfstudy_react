1c1d325d94b19977df7d2e33d4ee48c4f875c676
FerrariRoma <win9592@gmail.com>
2022-08-24 10:35:10 +0900

    ts-node설치
    별도의 빌드 없이 ts를 사용할 수 있다.

--- typescript/typechain/src/myPackage.js
+++ typescript/typechain/src/myPackage.js
@@ -2,9 +2,8 @@
 /**
  * Initializes the project
  * @param {object} config
- * @param {boolean} config.debug
  * @param {string} config.url
- * @returns void
+ * @returns boolean
  */
 export function init(config) {
   return true;
@@ -17,3 +16,9 @@ export function init(config) {
 export function exit(code) {
   return code + 1;
 }
+
+/* 
+이건 JSdoc이다. js를 ts에서 쓰는 방법이다. ts에서도 js를 쓸 수 있는데, js를 쓰기 위해서는 allowJs를 tsconfig.json에 설정해두면 된다.
+그리고 바로 써도 되지만 js를 ts로 리팩토링 하지 않고도 어느 정도의 call signature효과를 가질 수 있는데 그 방법이 JSdoc이다.
+
+*/
