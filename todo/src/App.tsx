3e5fd1495b9aa41b21843954203ad69c0f861b8b
FerrariRoma <win9592@gmail.com>
2022-09-24 19:50:06 +0900

    selector에서 set property 사용해보기

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,85 +1,35 @@
-import { useEffect } from "react";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import { createGlobalStyle, ThemeProvider } from "styled-components";
-import { isDarkAtom, todoList } from "./atoms";
-import Router from "./Router";
-import { darkTheme, lightTheme } from "./theme";
+import React from "react";
+import { useRecoilState } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
 
 function App() {
-  const isDark = useRecoilValue(isDarkAtom);
-  const setTodos = useSetRecoilState(todoList);
-  useEffect(() => {
-    const localTodos = localStorage.getItem("todos");
-    setTodos(localTodos ? JSON.parse(localTodos) : []);
-  }, []);
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const [hours, setHours] = useRecoilState(hourSelector);
+  // 이벤트, 리액트의 폼에서, HTML 인풋 엘리먼트에서 => 리액트 폼 안의 HTML인풋 엘리먼트에서 이벤트가 발생합니다.
+  const handleMinuteChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  };
+
+  const handleHourChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
+  };
+
   return (
     <>
-      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-        <GlobalStyle />
-        <Router />
-      </ThemeProvider>
+      <input
+        value={minutes}
+        onChange={handleMinuteChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input
+        value={hours}
+        onChange={handleHourChange}
+        type="number"
+        placeholder="Hours"
+      />
     </>
   );
 }
 
 export default App;
-
-const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Gamja+Flower&family=Source+Sans+Pro&display=swap');
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed, 
-figure, figcaption, footer, header, hgroup, 
-menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure, 
-footer, header, hgroup, menu, nav, section {
-	display: block;
-}
-body {
-	line-height: 1;
-}
-ol, ul {
-	list-style: none;
-}
-blockquote, q {
-	quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-	content: '';
-	content: none;
-}
-table {
-	border-collapse: collapse;
-	border-spacing: 0;
-}
-*{
-  box-sizing: border-box;
-}
-body{
-  font-family: 'Source Sans Pro', sans-serif;
-  background-color: ${(props) => props.theme.bgColor};
-  color: ${(props) => props.theme.textColor};
-  transition: background-color 0.3s linear, color 0.3s linear;
-}
-a{
-  text-decoration: none;
-  color: inherit;
-}
-`;
--- todo/src/atoms.ts
+++ todo/src/atoms.ts
@@ -13,43 +13,14 @@ export enum Categories {
 }
 이렇게 해주면 위에서 차례대로 0~2까지 numbering이 된다. 문제는 버튼의 name이 숫자여선 안된다는 것이다. string이여야 한다.
 */
-
-export enum Categories {
-  "TO_DO" = "TO_DO",
-  "DOING" = "DOING",
-  "DONE" = "DONE",
-}
-
-export interface ITodo {
-  text: string;
-  id: number;
-  category: Categories;
-}
-
-export const isDarkAtom = atom({
-  key: "isDark",
-  default: false,
-});
-
-export const todoList = atom<ITodo[]>({
-  key: "toDo",
-  default: [],
-});
-
-export const categoryState = atom<Categories>({
-  key: "category",
-  default: Categories.TO_DO,
-});
-
 /* 
     selector를 이용해서 todo들을 category에 따라서 분류해보도록 하자. 이게 selector의 파워이다.
     selector를 이용하면 state를 가져다가 다른 state를 만들 수 있다. 우리가 원하는 대로 state를 변형할 수 있다.
     우리로 치면, TO_DO state, DOING state, DONE state 각각 state를 만들긴 싫은것이다.
     하나의 atom에서 그 output을 더 잘 써먹는 방법인 것이다.
 */
-export const todoSelector = selector({
-  key: "todoSelector",
-  /* 
+
+/* 
   selector은 get function이 필요한데, 그 함수는 인자로 객체를 받는데, 그 객체에는 get Fucntion이 들어가 있다.
   이것은 selector가 기존 atom을 가져다가 output을 변형할 수 있다는 것인데, 그렇기 때문에 뭘 변형할 지 받아올 get Function이 있어야 한다.
   즉 그게 get function이 있어야 하는 이유이다. 이게 있어야지 atom을 받을 수 있다.
@@ -62,9 +33,24 @@ export const todoSelector = selector({
   그리고 그 객체에는 get function이 들어있다.
   get function을 이용하면 selector의 내부로 atom을 가지고 올 수 있다.
    */
-  get({ get }) {
-    const toDos = get(todoList);
-    const category = get(categoryState);
-    return toDos.filter((toDo) => toDo.category === category);
+
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
+});
+
+/* 복습 :: selector는 state를 가져다가 수정한 output을 return하는 것이다. */
+/* 오늘 새로 배울 건 set이다. 이건 State를 set하는 것을 도와준다.
+이건 atom을 수정하는 걸 도와준다. 즉 get을 이용해서 기존의 state를 selector에 가져오고,
+set은 state를 selector에서 수정한다. */
+export const hourSelector = selector<number>({
+  key: "hours",
+  get: ({ get }) => {
+    const minutes = get(minuteState);
+    return minutes / 60;
+  },
+  set: ({ set }, newValue) => {
+    const minutes = Number(newValue) * 60;
+    return set(minuteState, minutes);
   },
 });
